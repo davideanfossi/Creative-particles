3a84efa01beb7142f9e8076701e39444ebe20313
davideanfossi <51325306+davideanfossi@users.noreply.github.com>
2019-10-12 09:51:56 +0200

    update 5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// ------------------ MOUSE CLICK + NEW PARTICLES CHANGING COLOR ------------------
+// ------------------ FINAL VERSION ------------------
 class particle{
 
     constructor(){
@@ -60,6 +60,16 @@ var slider2 = document.getElementById("onOff");
 var onOff = slider2.checked;
 var radius = document.getElementById("radius");
 var colorSlider = document.getElementById("color");
+var rvs = document.getElementById("rvs");
+var revers = rvs.checked;
+var squareBox = document.getElementById("square");
+
+var lastX = 0, lastY = 0;
+var mx, my, lastmx, lastmy, setShape = 0;
+var speed = speedSlider.value * 0.05;
+var r = radius.value.value / 10;
+var colorSpeed = colorSlider.value / 100;
+var flag = false;
 
 function resize(){
     canvas.setAttribute("width", window.innerWidth - 200 + "px");
@@ -68,13 +78,10 @@ function resize(){
 }
 resize();
 
-var lastX = 0, lastY = 0;
-var mx, my, lastmx, lastmy, setShape = 0;
-
 function turn(){
     particles = [];
-    mx = -200;
-    my = -200;
+    mx = -600;
+    my = -600;
     onOff = slider2.checked;
     if (onOff) {
         canvas.style.cursor = "none";
@@ -83,24 +90,49 @@ function turn(){
     }
 }
 
-var speed = speedSlider.value * 0.05;
-var r = radius.value.value / 10;
-var colorSpeed = colorSlider.value / 100;
-console.log(colorSpeed);
+canvas.onmousedown = function(down){
+    if (down.button === 0){
+        flag = true;
+        mx = down.clientX;
+        my = down.clientY;
+    }   
+}
+
+canvas.onmouseup = function(up){
+    if (up.button === 0){
+        flag = false;
+        mx = -600;
+        my = -600;
+    }    
+}
+
+
 //setXY function is also used to update sliders values.
 function setXY(event){
-    mx = event.clientX;
-    my = event.clientY;
-    speed = speedSlider.value * 0.05;
-    r = radius.value / 10;
-    colorSpeed = colorSlider.value / 100;
+    if (flag) {
+        mx = event.clientX;
+        my = event.clientY;
+        speed = speedSlider.value * 0.05;
+        r = radius.value / 10;
+        colorSpeed = colorSlider.value / 100;
+    }
 }
 
 function clr(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    mx = -200;
-    my = -200;
+    mx = -600;
+    my = -600;
     particles = [];
+}
+
+function rst(){
+    speedSlider.value = 80;
+    radius.value = 120;
+    colorSlider.value = 50;
+    rvs.checked = false;
+    revers = false;
+    setShape = 0;
+    squareBox.checked = true;
 }
 
 function setCircle(){
@@ -109,6 +141,13 @@ function setCircle(){
 
 function setSquare(){
     setShape = 0;
+}
+
+function reverse(){
+    revers = rvs.checked;
+    particles = [];
+    mx = -600;
+    my = -600;
 }
 
 
@@ -125,7 +164,9 @@ function draw(){
             particles.push(p);
         }
         
-        //ctx.clearRect(0, 0, canvas.width, canvas.height);
+        if (revers) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
         for(var i = 0; i < particles.length; i++){
             particles[i].update();
             particles[i].show();
@@ -141,242 +182,3 @@ function draw(){
 
 
 
-// ------------------ MOUSE FOLLOW v2 + NEW PARTICLES CHANGING COLOR ------------------
-/* var mx, my, lastmx, lastmy, lastvx, lastvy;
-
-function setXY(event){
-    mx = event.clientX;
-    my = event.clientY;
-}
-
-var lastX = 0, lastY = 0;
-class particle{
-
-    constructor(){
-        this.x = mx;
-        this.y = my;
-        
-
-        if((mx - lastmx) !== 0){
-            this.vx = -(mx - lastmx) * 0.5;
-            this.vy = -(my - lastmy) * 0.5;
-        } else{
-            this.vx = lastvx;
-            this.vy = lastvy;
-        }
-        this.vx += -(Math.random() - 0.5) * 0.1;
-        this.vy += -(Math.random() - 0.5) * 0.1;
-
-        lastvx = this.vx;
-        lastvy = this.vy;
-
-        lastmx = this.x;
-        lastmy = this.y;
-
-        this.alpha = 1;
-        this.r = 0;
-        this.g = 0;
-        this.b = 0;
-        this.frequency = 0.06;
-        this.color = "";
-    }
-
-    update(){
-        this.x += this.vx;
-        this.y += this.vy;
-
-        this.alpha -= 0.018;
-    }
-
-    updateColor(i){
-        this.r = Math.sin(this.frequency*i + 0) * 127 + 128; 
-        this.g = Math.sin(this.frequency*i + 2) * 127 + 128; 
-        this.b = Math.sin(this.frequency*i + 4) * 127 + 128; 
-    }
-
-    show(){
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, 16, 0, 2*Math.PI, false);
-        this.color = "rgba(" + this.r + ", " + this.b + ", " + this.g + ", " + this.alpha + ")";
-        ctx.fillStyle = this.color;
-        ctx.fill();
-    }
-
-    finished(){
-        return this.alpha < 0;
-    }
-
-}
-
-var canvas = document.getElementById("canvas");
-ctx = canvas.getContext("2d");
-
-var particles = [];
-var i = 0;
-var j = 0;
-
-
-var id = setInterval(draw, 10);
-function draw(){
-    for(var k = 0; k < 3; k++){
-        let p = new particle();
-        p.updateColor(j);
-        particles.push(p);
-    }
-    
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for(var i = 0; i < particles.length; i++){
-        particles[i].update();
-        particles[i].show();
-        if (particles[i].finished()){
-            particles.splice(i, 1);
-        }
-    }
-    j++;
-} */
-
-
-// ------------------ MOUSE FOLLOW + NEW PARTICLES CHANGING COLOR ------------------
-/* let mx, my;
-
-function setXY(event){
-    mx = event.clientX;
-    my = event.clientY;
-}
-
-var lastX = 0, lastY = 0;
-class particle{
-
-    constructor(){
-        this.x = mx;
-        this.y = my;
-        
-        this.vx = Math.random()*2 - 1;
-        this.vy = Math.random()*4 - 5;
-
-        this.alpha = 1;
-        this.r = 0;
-        this.g = 0;
-        this.b = 0;
-        this.frequency = 0.06;
-        this.color = "";
-    }
-
-    update(){
-        this.x += this.vx;
-        this.y += this.vy;
-        this.alpha -= 0.012;
-    }
-
-    updateColor(i){
-        this.r = Math.sin(this.frequency*i + 0) * 127 + 128; 
-        this.g = Math.sin(this.frequency*i + 2) * 127 + 128; 
-        this.b = Math.sin(this.frequency*i + 4) * 127 + 128; 
-    }
-
-    show(){
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, 16, 0, 2*Math.PI, false);
-        this.color = "rgba(" + this.r + ", " + this.b + ", " + this.g + ", " + this.alpha + ")";
-        ctx.fillStyle = this.color;
-        ctx.fill();
-    }
-
-    finished(){
-        return this.alpha < 0;
-    }
-
-}
-
-var canvas = document.getElementById("canvas");
-ctx = canvas.getContext("2d");
-
-var particles = [];
-var i = 0;
-var j = 0;
-
-
-var id = setInterval(draw, 10);
-function draw(){
-    let p = new particle();
-    p.updateColor(j);
-    particles.push(p);
-    
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for(var i = 0; i < particles.length; i++){
-        particles[i].update();
-        particles[i].show();
-        if (particles[i].finished()){
-            particles.splice(i, 1);
-        }
-    }
-    j++;
-} */
-
-
-
-
-
-// ------------------ ALL PARTICLES CHANGING COLOR ------------------
-/* class particle{
-
-    constructor(){
-        this.x = 300;
-        this.y = 550;
-        this.vx = Math.random()*2 - 1;
-        this.vy = Math.random()*4 - 5;
-        this.alpha = 1;
-        this.r = 0;
-        this.g = 0;
-        this.b = 0;
-        this.frequency = 0.06;
-        this.color = "";
-    }
-
-    update(i){
-        this.x += this.vx;
-        this.y += this.vy;
-        this.alpha -= 0.012;
-        this.r = -1 * Math.sin(this.frequency*i + 0) * 127 + 128; 
-        this.g = -1 * Math.sin(this.frequency*i + 2) * 127 + 128; 
-        this.b = -1 * Math.sin(this.frequency*i + 4) * 127 + 128; 
-    }
-
-    show(){
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, 16, 0, 2*Math.PI, false);
-        this.color = "rgba(" + this.r + ", " + this.b + ", " + this.g + ", " + this.alpha + ")";
-        ctx.fillStyle = this.color;
-        ctx.fill();
-    }
-
-    finished(){
-        return this.alpha < 0;
-    }
-
-}
-
-var canvas = document.getElementById("canvas");
-ctx = canvas.getContext("2d");
-
-var particles = [];
-var i = 0;
-var j = 0;
-
-
-var id = setInterval(draw, 20);
-function draw(){
-    let p = new particle();
-    //p.updateColor(j);
-    particles.push(p);
-    
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for(var i = 0; i < particles.length; i++){
-        particles[i].update(j);
-        particles[i].show();
-        if (particles[i].finished()){
-            particles.splice(i, 1);
-        }
-    }
-    j++;
-} */
